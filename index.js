40b7125e71f327ca814f8891ce4146c5c3ffc2b9
erickseis <erickseis@gmail.com>
2022-09-01 00:19:54 -0300

    3ra carga

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 //*settings
 app.set('port', process.env.PORT || 3000);
@@ -9,6 +10,7 @@ app.set('json spaces', 2);
 //*midelwares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 //*routes
 app.use(require('./routes/products.routes'));
